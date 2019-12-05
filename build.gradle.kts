bd89d5bdde8f7c875a5386ead1200cb82003104b
Wellington Costa <wellington.costa128@gmail.com>
2019-12-05 15:36:32 -0300

    Updating Kotlin plugin version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
         classpath("org.flywaydb:flyway-gradle-plugin:6.0.4")
     }
 }
